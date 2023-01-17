69b1c7605d2f191ed193a693148c9b1ab9b62d09
Snorre Ledal <snorreledal@hotmail.no>
2023-01-17 21:12:40 +0100

    Ledlight dicemachin done

--- src/main.cpp
+++ src/main.cpp
@@ -11,13 +11,13 @@
  * */
 
 /* Led-light ID */
-const int pinOutLed1 = 13;
-const int pinOutLed2 = 12;
-const int pinOutLed3 = 11;
-const int pinOutLed4 = 10;
-const int pinOutLed5 = 9;
-const int pinOutLed6 = 8;
-const int pinOutLed7 = 7;
+int pinOutLed1 = 13;
+int pinOutLed2 = 12;
+int pinOutLed3 = 11;
+int pinOutLed4 = 10;
+int pinOutLed5 = 9;
+int pinOutLed6 = 8;
+int pinOutLed7 = 7;
 
 /* Speaker */
 const int pinOutSpeaker = 3;
@@ -43,7 +43,7 @@ int melody[] = {
 
         NOTE_A3, NOTE_B3, NOTE_CS4, NOTE_DS4,
         NOTE_A3, NOTE_B3, NOTE_C4, NOTE_D4,
-        NOTE_AS3, NOTE_C4,NOTE_D4, NOTE_E4,
+        NOTE_AS3, NOTE_C4, NOTE_D4, NOTE_E4,
         NOTE_B3, NOTE_C4, NOTE_D4, NOTE_E4,
 
         NOTE_B3, NOTE_CS4, NOTE_DS4, NOTE_F4,
@@ -75,7 +75,14 @@ int noteDurations[] = {
         2, 2, 2, 1
 };
 
-void rollDiceSong();
+
+void showDice(long number);
+
+void hideDice();
+
+void sensorAndLedTest();
+
+void numberTest();
 
 void setup() {
     Serial.begin(9600);
@@ -97,27 +104,73 @@ void loop() {
      * 0 == active */
     int stateSensor = digitalRead(pinInnSensor);
 
-//    tone(pinOutSpeaker, BREAK);
+//    sensorAndLedTest();
+//    numberTest();
 
-    rollDiceSong();
+    if (stateSensor == 0) {
+        for (int thisNote = 0; thisNote < 54; thisNote++) {
+            hideDice();
+            int noteDuration = 600 / noteDurations[thisNote];
 
+            long number = random(0, 6);
+            showDice(number);
+            Serial.print('\n');
+            Serial.print(number);
+            Serial.print('\n');
 
-    Serial.print(stateSensor);
-    Serial.print('\n');
-    delay(100);
+            tone(3, melody[thisNote], noteDuration);
+
+            int pauseBetweenNotes = noteDuration * 1.30;
+            delay(pauseBetweenNotes);
+            noTone(3);
+        }
+
+        hideDice();
+        long number = random(0, 6);
+        showDice(number);
+    }
+
 }
 
-void rollDiceSong() {
-
-    for (int thisNote = 0; thisNote < 54; thisNote++) {
-
-        int noteDuration = 600 / noteDurations[thisNote];
-        tone(3, melody[thisNote], noteDuration);
-
-        int pauseBetweenNotes = noteDuration * 1.30;
-        delay(pauseBetweenNotes);
-        noTone(3);
+void showDice(long number) {
+    for (int i = 0; i < 7; ++i) {
+        if (dice[number][i] == 1) {
+            Serial.print(i);
+            switch (i) {
+                case 0:
+                    digitalWrite(pinOutLed1, 1);
+                    break;
+                case 1:
+                    digitalWrite(pinOutLed2, 1);
+                    break;
+                case 2:
+                    digitalWrite(pinOutLed3, 1);
+                    break;
+                case 3:
+                    digitalWrite(pinOutLed4, 1);
+                    break;
+                case 4:
+                    digitalWrite(pinOutLed5, 1);
+                    break;
+                case 5:
+                    digitalWrite(pinOutLed6, 1);
+                    break;
+                case 6:
+                    digitalWrite(pinOutLed7, 1);
+                    break;
+            }
+        }
     }
+}
+
+void hideDice() {
+    digitalWrite(pinOutLed1, 0);
+    digitalWrite(pinOutLed2, 0);
+    digitalWrite(pinOutLed3, 0);
+    digitalWrite(pinOutLed4, 0);
+    digitalWrite(pinOutLed5, 0);
+    digitalWrite(pinOutLed6, 0);
+    digitalWrite(pinOutLed7, 0);
 }
 
 void sensorAndLedTest() {
@@ -147,3 +200,40 @@ void sensorAndLedTest() {
     Serial.print('\n');
     delay(100);
 }
+
+void numberTest() {
+    for (int number = 0; number < 6; ++number) {
+        for (int i = 0; i < 7; ++i) {
+            if (dice[number][i] == 1) {
+                Serial.print(i);
+                switch (i) {
+                    case 0:
+                        digitalWrite(pinOutLed1, 1);
+                        break;
+                    case 1:
+                        digitalWrite(pinOutLed2, 1);
+                        break;
+                    case 2:
+                        digitalWrite(pinOutLed3, 1);
+                        break;
+                    case 3:
+                        digitalWrite(pinOutLed4, 1);
+                        break;
+                    case 4:
+                        digitalWrite(pinOutLed5, 1);
+                        break;
+                    case 5:
+                        digitalWrite(pinOutLed6, 1);
+                        break;
+                    case 6:
+                        digitalWrite(pinOutLed7, 1);
+                        break;
+                }
+            }
+        }
+        delay(1000);
+        hideDice();
+        delay(1000);
+    }
+}
+
